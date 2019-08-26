3c70f9ac2058b6eb642f5fda0d1b878aef37ab38
eliud981 <eliuddelgado40@gmail.com>
2019-08-25 19:38:32 -0500

    Practica 1,2,3 y 4

--- main.c
+++ main.c
@@ -1,8 +1,116 @@
-#include <stdio.h>
-#include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//PROBLEMA 1
+/*
+*Autor: Eliud Y. Delgado Bedoy
+*Fecha: 24/08/2019
+*Grupo: 006
+*Horario: 7:00am - 9:00am.
+*Matricula: 1737570
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 
-int main(int argc, char *argv[]) {
-	return 0;
+int main() //inicio de programa
+{
+	//Definimos variables de tipo flotante para que el usuario sea libre de poner la cantidad que sea y por si el reultado no es entero 
+	float x;
+	float resultado;
+	//Pedimos que introduzca cualquier valor para nuestra variable 
+	printf("Introduzca algun valor para ""x"": ");
+	scanf("%f" ,&x);
+	
+	resultado = (x-1)/4 - (x-5)/36; //ponemos la expresion algebraica para dar resultado
+	printf("El resultado es: %f", resultado);//impresion del reultado
+	
+	
+	return 0;//fin de programa
 }
+
+//PRONLEMA 2
+/*
+*Autor: Eliud Y. Delgado Bedoy
+*Fecha: 24/08/2019
+*Grupo: 006
+*Horario: 7:00am - 9:00am.
+*Matricula: 1737570
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main() //inicio de programa
+{
+	//Definimos variables de tipo flotante para que el usuario sea libre de poner la cantidad que sea y por si el reultado no es entero 
+	float x;
+	float resultado;
+	//Pedimos que introduzca cualquier valor para nuestra variable 
+	printf("Introduzca algun valor para ""x"": ");
+	scanf("%f" ,&x);
+	resultado = 6*((x+1)/8 - (2*x-3)/16); //ponemos la expresion algebraica para dar resultado
+	printf("El resultado es: %f", resultado);//impresion del reultado
+	
+	
+	return 0;//fin de programa
+}
+
+//PROBLEMA 3
+/*
+*Autor: Eliud Y. Delgado Bedoy
+*Fecha: 24/08/2019
+*Grupo: 006
+*Horario: 7:00am - 9:00am.
+*Matricula: 1737570
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main() //inicio de programa
+{
+	//Definimos variables de tipo flotante para que el usuario sea libre de poner la cantidad que sea y por si el reultado no es entero 
+	float x;
+	float resultado;
+	//Pedimos que introduzca cualquier valor para nuestra variable 
+	printf("Introduzca algun valor para ""x"": ");
+	scanf("%f" ,&x);
+	
+	resultado = 2/3*(x-(1-(x+2)/3))+1; //ponemos la expresion algebraica para dar resultado
+	printf("El resultado es: %f", resultado);//impresion del reultado
+	
+	
+	return 0;//fin de programa
+}
+
+
+//PROBLEMA 4
+/*
+*Autor: Eliud Y. Delgado Bedoy
+*Fecha: 24/08/2019
+*Grupo: 006
+*Horario: 7:00am - 9:00am.
+*Matricula: 1737570
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main() //inicio de programa
+{
+	//Definimos variables de tipo flotante para que el usuario sea libre de poner la cantidad que sea y por si el reultado no es entero 
+	float x;
+	float resultado;
+	//Pedimos que introduzca cualquier valor para nuestra variable 
+	printf("Introduzca algun valor para ""x"": ");
+	scanf("%f" ,&x);
+	
+	resultado = 2-(-2*(x+1)-(x-3)/2); //ponemos la expresion algebraica para dar resultado
+	printf("El resultado es: %f", resultado);//impresion del reultado
+	
+	
+	return 0;//fin de programa
+}
+
+
+
+
