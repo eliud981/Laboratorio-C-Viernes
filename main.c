c970e02b259897a08c7a3a5d3714c8e9854f4ad3
eliud981 <eliuddelgado40@gmail.com>
2019-10-26 20:33:09 -0500

    Practica12

--- main.c
+++ main.c
@@ -1,8 +1,41 @@
+/*
+*Autor: Eliud Y. Delgado Bedoy
+*Fecha: 24/08/2019
+*Grupo: 006
+*Horario: 7:00am - 9:00am.
+*Matricula: 1737570
+*/
 #include <stdio.h>
-#include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	return 0;
+#include <math.h>
+ 
+int main ()
+{
+	/**************************ACTIVIDAD 1******************/
+	printf("\n\n\t\t\t*********************ACTIVIDAD 1********************* \n\n");
+	//declaracion de variables 
+  float exponente;
+  float potencia;
+  printf( "Escriba el exponente: " );//
+  scanf( "%f", &exponente);
+  printf( "Escriba la potencia: " );
+  scanf( "%f", &potencia);
+  printf("%f elevado a %f es %lf\n", exponente, potencia, pow (exponente, potencia));//efectuamos operacion necesaria
+  
+  
+  /**************************ACTIVIDAD 2******************/
+	printf("\n\n\t\t\t*********************ACTIVIDAD 2********************* \n\n");
+  int n,i,c;//declaracion de variables
+  printf("\n\nIngrese un numero: ");//introduce numero el usuario
+  scanf("%d",&n);//escaneamos el numero
+  
+  for(i=1;i<=n;i++)//ciclamos 
+  {
+  	if(n%i==0)c++;//condicion de que si es igual a 0
+  }
+  if(c==3)//si es igual a 3
+  printf("El Numero es primo");//el numero es primo
+  else//si no 
+  printf("El Numero no es primo");//el numero no es primo 
+  
+  
 }
